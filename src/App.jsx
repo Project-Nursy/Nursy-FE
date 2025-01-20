37f12e96cdb6bb22173fe792334671035ca457ff
kwonjieon <holemart@naver.com>
2025-01-20 17:54:26 +0900

    프리티어적용 + 이슈,풀리퀘스트 템플릿

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import LoginPage from "./test/login";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SignupPage from "./test/signUp";
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import LoginPage from './test/login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignupPage from './test/signUp';
 
 function App() {
   return (
