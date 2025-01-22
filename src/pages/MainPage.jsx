41a8e882f4b6f33ee0382b53804baf82c524506e
kwonjieon <holemart@naver.com>
2025-01-22 14:37:19 +0900

    feat: 로그인 페이지 , 메인페이지

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,10 @@
+function MainPage() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to Nursy</h1>
+      <p>Your healthcare scheduling companion!</p>
+    </div>
+  );
+}
+
+export default MainPage;
