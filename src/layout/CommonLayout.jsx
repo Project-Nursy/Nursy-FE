902061ab542e735cc0fceaad378350ed272d2e72
kwonjieon <holemart@naver.com>
2025-01-25 20:40:24 +0900

    feat: 사이드 바 구현 및 탭 페이지 생성

--- src/layout/CommonLayout.jsx
+++ src/layout/CommonLayout.jsx
@@ -1,15 +1,50 @@
+// import Header from '../components/Header';
+// import { Outlet, useLocation } from 'react-router-dom';
+// import SideBar from '../components/SideBar';
+// import { Container } from 'react-bootstrap';
+
+// export default function CommonLayout() {
+//   const location = useLocation();
+//   const ward = location.state?.ward; // ward 객체를 받아옵니다.
+
+//   return (
+//     <div>
+//       <Header />
+//       <div className="flex h-[88vh]">
+//         <SideBar ward={ward} />
+//         <main>
+//           <Container>
+//             <div>
+//               <Outlet />
+//             </div>
+//           </Container>
+//         </main>
+//       </div>
+//     </div>
+//   );
+// }
 import Header from '../components/Header';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import SideBar from '../components/SideBar';
 import { Container } from 'react-bootstrap';
 
 export default function CommonLayout() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const ward = location.state?.ward; // ward 객체를 받아옵니다.
+
+  // ward 객체가 없을 경우 메인 페이지로 리다이렉트
+  if (!ward) {
+    navigate('/main');
+    return null;
+  }
+
   return (
     <div>
       <Header />
-      <div>
-        <SideBar />
-        <main>
+      <div className="flex h-[88vh]">
+        <SideBar ward={ward} />
+        <main className="flex-1">
           <Container>
             <div>
               <Outlet />
---
+++ src/pages/WardMainPage.jsx
@@ -0,0 +1,7 @@
+export default function WardMainPage() {
+  return (
+    <div>
+      <h1>Ward Main Page</h1>
+    </div>
+  );
+}
